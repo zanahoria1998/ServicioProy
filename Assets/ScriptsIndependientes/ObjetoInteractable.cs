8b8f10681b7b80fe2e72a5e9c9db3b370c86c224
Zanahoria1998 <mindy.muse@hotmail.com>
2023-07-03 11:52:07 -0600

    Ya está al 100% 1er y 2do nivel, falta hacer los demás niveles

--- Assets/ScriptsIndependientes/ObjetoInteractable.cs
+++ Assets/ScriptsIndependientes/ObjetoInteractable.cs
@@ -6,15 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class ObjetoInteractable : MonoBehaviour
 {
+    public int i;
     public Textos textos; //Refereciando al Script Textos
 
     public void OnMouseDown(){ //Al presionar un objeto que tenga este script
 
         FindObjectOfType<ControlDialogo>().ActivarCartel(textos);//buscará el objeto que tenga el control de dialógo y posteriormente recurrirá al método de activar cartel
-        //Debug.Log("Llamada a ObjetoInteractable");
+
     }
 
-    public void SiguienteNivel(){
-        SceneManager.LoadScene(1);
+    public void SiguienteNivel(int i){ 
+        SceneManager.LoadScene(i);
     }
 }
+
